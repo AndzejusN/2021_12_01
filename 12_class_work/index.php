c0bc4ad2a029107462df1940cfdee45ee817da53
Andrew <90209255+AndzejusN@users.noreply.github.com>
2021-12-21 20:53:24 +0200

    12 class work_5

--- 12_class_work/index.php
+++ 12_class_work/index.php
@@ -3,7 +3,7 @@
 $fileJs = file_get_contents('https://randomuser.me/api/');
 $data = json_decode($fileJs, true);
 
-file_put_contents("new.json", json_encode($data['results']));
+file_put_contents("new.json", json_encode($data['results'][0]));
 
 
 $data = file_get_contents('new.json');
