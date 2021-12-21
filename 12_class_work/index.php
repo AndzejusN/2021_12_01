494483e98fe8169427e562d94c3941354c38fecd
Andrew <90209255+AndzejusN@users.noreply.github.com>
2021-12-21 20:42:05 +0200

    12 class work_4

--- 12_class_work/index.php
+++ 12_class_work/index.php
@@ -3,17 +3,17 @@
 $fileJs = file_get_contents('https://randomuser.me/api/');
 $data = json_decode($fileJs, true);
 
-file_put_contents("new.json", json_encode($data['result']));
+file_put_contents("new.json", json_encode($data['results']));
 
 
 $data = file_get_contents('new.json');
-$newData = json_decode($data, true);
+$data = json_decode($data, true);
 
 $csvFileName = 'new.csv';
 
 $fp = fopen($csvFileName, 'w');
 
-foreach ($newData as $row){
+foreach ($data as $row){
     fputcsv($fp, $row);
 }
 
