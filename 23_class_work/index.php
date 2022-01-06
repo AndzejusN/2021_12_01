87b0a4e617c6e658acc3247896fa78014aa33e11
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-01-06 21:19:05 +0200

    23_2 Class work

--- 23_class_work/index.php
+++ 23_class_work/index.php
@@ -2,6 +2,13 @@
 
 require_once 'classes/Tag.php';
 
+$tag = new Tag('div');
+
+$tag->setText('title')->setAttr('style', 'background-color:#8ebf42')->show();
+// prints <a href="index.html">title</a>
+$tag->setText('text')->setAttr('style', 'background-color:#8ebf42')->get();
+// returns <a href="index.html">text</a>
+
 $tag = new Tag('a');
 
 $tag->setText('title')->setAttr('href', 'index.html')->show();
