2e9f92d74099dab6fb1f09d3032615a4c430fa6e
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-01-25 18:26:51 +0200

    WeatherAppMain_2

--- WeatherApp-main/public/index.php
+++ WeatherApp-main/public/index.php
@@ -5,54 +5,39 @@ require_once '../vendor/autoload.php';
 
 (Dotenv\Dotenv::createImmutable(ROOT_PATH))->load();
 
-
 use Sunrise\Http\Message\ResponseFactory;
 use Sunrise\Http\Router\RequestHandler\CallableRequestHandler;
 use Sunrise\Http\Router\RouteCollector;
 use Sunrise\Http\Router\Router;
 use Sunrise\Http\ServerRequest\ServerRequestFactory;
-use App\Controller\HomeController as HomeRequestHandler;
 use function Sunrise\Http\Router\emit;
-
 
 $collector = new RouteCollector();
 
 $collector->get('home', '/', new CallableRequestHandler(function () {
-    require_once ROOT_PATH . '/views/index.html';
-    return (new ResponseFactory)->createResponse(200);
+    $apiData = file_get_contents(ROOT_PATH . "/views/index.html");
+    return (new ResponseFactory)->createHtmlResponse(200, $apiData);
 }));
-
-
-//$collector->get('home', '/', [HomeRequestHandler::class, 'index']);
-
-//$collector->get('home', '/', [HomeRequestHandler::class, 'index']);
-
-//$collector->get('home', '/', new HomeRequestHandler())
-
-//$collector->get('index', '/', new CallableRequestHandler(function ($request) {
-//
-//$apiData = include('../views/index.html');
-//    return (new ResponseFactory)->createHtmlResponse(200,'../views/index.html');
-//}));
-
 
 $collector->get('places', '/weather/places', new CallableRequestHandler(function () {
     $apiData = file_get_contents("https://api.meteo.lt/v1/places");
-    return (new ResponseFactory)->createJsonResponse(200, $apiData);
+    $apiData = json_decode($apiData);
+    return (new ResponseFactory)->createJsonResponse(200, $apiData, JSON_PRETTY_PRINT);
 }));
 
+$collector->get('places by city', '/weather/places/{id}', new CallableRequestHandler(function ($request) {
+    $city = $request->getAttribute('id');
+    $apiData = file_get_contents("https://api.meteo.lt/v1/places/" . $city);
+    $apiData = json_decode($apiData);
+    return (new ResponseFactory)->createJsonResponse(200, $apiData, JSON_PRETTY_PRINT);
+}));
 
-//$collector->get('places by city', '/weather/places/{id}', new CallableRequestHandler(function ($request) {
-//    $city = $request->getAttribute('id');
-//    $apiData = file_get_contents("https://api.meteo.lt/v1/places/" . $city);
-//    return (new ResponseFactory)->createJsonResponse(200, $apiData);
-//}));
-//
-//$collector->get('places by city long term', '/weather/long-term/{places}', new CallableRequestHandler(function ($request) {
-//    $city = $request->getAttribute('places');
-//    $apiData = file_get_contents("https://api.meteo.lt/v1/places/" . $city . "/forecasts/long-term");
-//    return (new ResponseFactory)->createJsonResponse(200, $apiData);
-//}));
+$collector->get('places by city long term', '/weather/long-term/{places}', new CallableRequestHandler(function ($request) {
+    $city = $request->getAttribute('places');
+    $apiData = file_get_contents("https://api.meteo.lt/v1/places/" . $city . "/forecasts/long-term");
+    $apiData = json_decode($apiData);
+    return (new ResponseFactory)->createJsonResponse(200, $apiData, JSON_PRETTY_PRINT);
+}));
 
 $router = new Router();
 $router->addRoute(...$collector->getCollection()->all());
