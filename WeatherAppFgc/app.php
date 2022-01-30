eeaf269eee55c0032e37133f1e24f4b0519d6a54
Andrew <90209255+AndzejusN@users.noreply.github.com>
2022-01-30 15:35:19 +0200

    Class Work WeatherAppFCG_

--- WeatherAppFgc/app.php
+++ WeatherAppFgc/app.php
@@ -1,15 +1,15 @@
 <?php
 
-$city = $_POST['city'] ?? 'Vilnius';
+$city = htmlspecialchars($_POST['city']) ?? 'Vilnius';
 
 $content = @file_get_contents("https://api.meteo.lt/v1/places/" . $city . "/forecasts/long-term");
 
 if (!$content) {
     $apiData = file_get_contents("https://api.meteo.lt/v1/places/vilnius/forecasts/long-term");
 
-    $div1 = "<div class=\"alert alert-warning\" role=\"alert\">";
-    $error = 'Paieškos lauke klaidingai įvestas miesto pavadinimas';
-    $div2 = "</div>";
+    $error = "<div class=\"alert alert-warning\" role=\"alert\">";
+    $error .= 'Paieškos lauke klaidingai įvestas miesto pavadinimas';
+    $error .= "</div>";
 
 } else {
     $apiData = file_get_contents("https://api.meteo.lt/v1/places/" . $city . "/forecasts/long-term");
